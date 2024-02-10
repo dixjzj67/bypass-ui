2300f2f6c03513b7b90ffe1f7a2980d1b07fe29b
dixjzj67 <146504948+dixjzj67@users.noreply.github.com>
2024-02-10 12:05:38 +0800

    Update JAva.js

--- Java.js
+++ Java.js
@@ -9,7 +9,7 @@ var AWN;
                 const i = {
                     maxNotifications: 10,
                     animationDuration: 300,
-                    position: "bottom-left",
+                    position: "center",
                     labels: {
                         tip: "Tip",
                         info: "执行中…",
